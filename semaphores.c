60052da83ba11f3d39cfaa6af3d1e579a5539596
Prajnahebbar19 <158985324+Prajnahebbar19@users.noreply.github.com>
2024-06-12 12:22:18 +0530

    Add files via upload

---
+++ semaphores.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+int mutex = 1;
+int full = 0;
+int empty = 10, s = 0;
+void wait()
+{
+   --mutex;
+}
+void signal()
+{
+    ++mutex;
+}
+
+
+void producer()
+{
+    wait();
+    ++full;
+
+    --empty;
+
+    s++;
+    printf("\nProducer produces item %d",s);
+    signal();
+}
+
+
+void consumer()
+{
+
+   wait();
+
+
+    --full;
+
+
+    ++empty;
+    printf("\nConsumer consumes item %d",s);
+    s--;
+
+
+   signal();
+}
+
+
+int main()
+{
+    int n, i;
+    printf("\n Enter 1 for Producer"
+           "\n Enter 2 for Consumer"
+           "\n Enter 3 for Exit");
+
+
+    for (i = 1; i > 0; i++) {
+
+        printf("\nEnter your choice:");
+        scanf("%d", &n);
+
+        switch (n) {
+        case 1:
+
+            if ((mutex == 1)
+                && (empty != 0)) {
+                producer();
+            }
+
+            else {
+                printf("Buffer is full!");
+            }
+            break;
+
+        case 2:
+            if ((mutex == 1)
+                && (full != 0)) {
+                consumer();
+            }
+
+            else {
+                printf("Buffer is empty!");
+            }
+            break;
+
+        case 3:
+            exit(0);
+            break;
+        }
+    }
+    return 0;
+}
